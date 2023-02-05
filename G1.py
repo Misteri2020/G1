8198e886c5df66fd2393ca217c012d29607f1a9b
Misteri2020 <124504640+Misteri2020@users.noreply.github.com>
2023-02-05 20:26:03 +0700

    Add files via upload

---
+++ G1.py
@@ -0,0 +1,14 @@
+import random
+
+import sys
+
+import time
+def mengetik(s):
+   for c in s + '\n':
+	sys.stdout.write(c)
+        sys.stdout.flush()
+
+
+	time.sle(random.random() * 0.3)
+
+mengetik('jangan lupa subscribe chanel1 misteri2020 \nselamat menonton\n Terima Kasih.')
